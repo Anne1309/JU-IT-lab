3cce88631811dba4c0ef15ab4eba976a2e759de0
Anne1309 <anuskanath1309@gmail.com>
2024-09-10 10:25:41 +0530

    Create a1q2.cpp

---
+++ Sem3/OOPS/Assignment1/a1q2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cout<<"enter the two numbers: ";
+    cin>>m>>n;
+    int sum;
+    sum=m+n;
+    cout<<"m= "<<m<<"n= "<<n<<endl;
+    cout<<"The sum is: "<<sum;
+    return 0;
+
+}
