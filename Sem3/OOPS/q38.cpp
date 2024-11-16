a5aa91915505f5acf49e1ae9f25f509309879adc
Anuska Nath <anuskanath1309@gmail.com>
2024-11-16 14:18:28 +0530

    Create q38.cpp

---
+++ Sem3/OOPS/q38.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Complex
+{
+private:
+    double real, imag;
+
+public:
+    Complex(double r = 0.0, double i = 0.0)
+    {
+        real = r;
+        imag = i;
+    }
+
+    friend ostream &operator<<(ostream &os, Complex &c)
+    {
+        if (c.imag >= 0)
+            os << c.real << " + " << c.imag << "i";
+        else
+            os << c.real << " - " << -c.imag << "i";
+        return os;
+    }
+};
+
+class Quadratic
+{
+private:
+    double a, b, c;
+
+public:
+    Quadratic(){
+        a=0;
+        b=0;
+        c=0;
+    }    Quadratic(double a, double b, double c){
+        this->a=a;
+        this->b=b;
+        this->c=c;
+    }
+
+    Quadratic operator+(Quadratic &q)
+    {
+        return Quadratic(a + q.a, b + q.b, c + q.c);
+    }
+
+    friend ostream &operator<<(ostream &os, Quadratic &q)
+    {
+        os << q.a << "x^2 + " << q.b << "x + " << q.c;
+        return os;
+    }
+
+    friend istream &operator>>(istream &is, Quadratic &q)
+    {
+        is >> q.a >> q.b >> q.c;
+        return is;
+    }
+
+    double evaluate(double x)
+    {
+        return a * x * x + b * x + c;
+    }
+
+    void computeRoots()
+    {
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant > 0)
+        {
+            double root1 = (-b + sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - sqrt(discriminant)) / (2 * a);
+            cout << "Real roots: " << root1 << " and " << root2 << endl;
+        }        {
+            double root = -b / (2 * a);
+            cout << "One real root: " << root << endl;
+        }
+        else
+        {
+            Complex root1((-b) / (2 * a), sqrt(-discriminant) / (2 * a));
+            Complex root2((-b) / (2 * a), -sqrt(-discriminant) / (2 * a));
+            cout << "Complex roots: " << root1 << " and " << root2 << endl;
+        }
+    }
+};
+
+int main()
+{
+    Quadratic q1(1, -3, 2);
+    Quadratic q2(1, 2, 1);
+    Quadratic sum = q1 + q2;
+
+    cout << "q1: " << q1 << endl;
+    cout << "q2: " << q2 << endl;
+    cout << "Sum: " << sum << endl;
+
+    cout << "q1 evaluated at x=1: " << q1.evaluate(1) << endl;
+
+    cout << "Roots of q1: ";
+    q1.computeRoots();
+
+    return 0;
+}
+
+        else if (discriminant == 0)
+
